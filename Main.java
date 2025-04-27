23e9fa60aebf0baa7cb0dd249991d4f42ae56000
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-04-27 17:11:38 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,35 +5,39 @@ public class Main {
       Tabuleiro tabuleiro = new Tabuleiro();
       Scanner input = new Scanner(System.in);
       int comando = -1;
-      String coordenada = "";
+      String coordenada_peca = "";
+      String coordenada_destino = "";
       int jogadorAtual = 1;
 
       
       System.out.println("Digite o comando, jogador" + jogadorAtual);
       System.out.println("1 - Movimento; 2 - Ataque; 3 - Mostrar tabuleiro");
       comando = Integer.parseInt(input.nextLine());
-      while (comando != 0 && !coordenada.equals("0"))
+      while (comando != 0 && !coordenada_peca.equals("0"))
       {
     	  if (comando == 1) {
     		  System.out.println("Escolha a peça (digite a coordenada):");
     		  System.out.println("Se quiser mostrar o tabuleiro, aperte 1");
     		  
-    		  coordenada = input.nextLine();
+    		  coordenada_peca = input.nextLine();
     		  
-    		  while (coordenada.equals("1")) 
+    		  while (coordenada_peca.equals("1")) 
     		  {
     			  
     			  tabuleiro.printaTabuleiro();
     			  System.out.println("Escolha a peça (digite a coordenada):");
         		  System.out.println("Se quiser mostrar o tabuleiro, aperte 1");
-    			  coordenada = input.nextLine();
+    			  coordenada_peca = input.nextLine();
     		  }
     		  
     		  
-    		  Pecas peca_escolhida = tabuleiro.getPecaNaPosicao(coordenada);
+    		  Pecas peca_escolhida = tabuleiro.getPecaNaPosicao(coordenada_peca);
+    		  System.out.println("Escolha em que posicao ir (digite a coordenada):");
+    		  System.out.println("Se quiser mostrar o tabuleiro, aperte 1");
+			  coordenada_destino = input.nextLine();
     		  //Peao peao = (Peao)peca_escolhida;
     		  //System.out.println(peca_escolhida.getClass());
-    		  tabuleiro.movimento(peca_escolhida,tabuleiro,coordenada);
+    		  tabuleiro.movimento(peca_escolhida,tabuleiro,coordenada_peca, coordenada_destino);
     		  tabuleiro.printaTabuleiro();
     		  jogadorAtual = 2;
     		  
