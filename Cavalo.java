ca5e5d1f335d39df8d2d0ae32baff979ed96dfbe
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-05-01 15:50:44 -0300

    Update Cavalo.java

--- Cavalo.java
+++ Cavalo.java
@@ -1,9 +1,11 @@
 public class Cavalo extends Pecas {
 
   private String cavalo_do_jogador;
+  public int jogador;
   
-  public Cavalo(String cavalo_J){
+  public Cavalo(String cavalo_J, int jogador_1_ou_2){
     cavalo_do_jogador = cavalo_J;
+    jogador = jogador_1_ou_2;
   }
   
 
