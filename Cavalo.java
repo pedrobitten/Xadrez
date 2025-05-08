8cca5617bcdef9189ad9a1fbd34b0cbfd5302c5c
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-05-08 19:44:52 -0300

    Update Cavalo.java

--- Cavalo.java
+++ Cavalo.java
@@ -52,5 +52,45 @@ public class Cavalo extends Pecas {
 		}
 	  
   }
+  
+  public void ataqueCavalo(Tabuleiro tabuleiro, Cavalo cavalo_jogador, String coordenada_cavalo, String coordenada_peca_inimiga)
+  {
+	  char colunaChar_cavalo = coordenada_cavalo.toLowerCase().charAt(0);
+	  char linhaChar_cavalo = coordenada_cavalo.charAt(1);
+
+	  int coluna_cavalo = colunaChar_cavalo - 'a';
+	  int linha_cavalo = 8 - Character.getNumericValue(linhaChar_cavalo);
+		
+	  char colunaChar_peca_inimiga = coordenada_peca_inimiga.toLowerCase().charAt(0);
+	  char linhaChar_peca_inimiga = coordenada_peca_inimiga.charAt(1);
+
+	  int coluna_peca_inimiga = colunaChar_peca_inimiga - 'a';
+	  int linha_peca_inimiga = 8 - Character.getNumericValue(linhaChar_peca_inimiga);
+		
+	  int diferenca_linha = Math.abs(linha_peca_inimiga - linha_cavalo);
+	  int diferenca_coluna = Math.abs(coluna_peca_inimiga - coluna_cavalo);
+	  
+	  
+	  if ((diferenca_linha == 2 && diferenca_coluna == 1) || (diferenca_linha == 1 && diferenca_coluna == 2))
+	  {
+		  Pecas peca_inimiga = tabuleiro.getPecaNaPosicao(coordenada_peca_inimiga);
+		  
+		  if (peca_inimiga.getJogador() == cavalo_jogador.getJogador()) {
+			  System.out.println("Movimento inválido! O Cavalo não pode atacar");
+			  return ;
+		  }
+		  
+		  tabuleiro.matriz[linha_cavalo][coluna_cavalo] = null;
+		  tabuleiro.matriz[linha_peca_inimiga][coluna_peca_inimiga] = cavalo_jogador;
+		  	
+		  	
+	  }
+		
+	  else {
+		  System.out.println("Movimento invalido para o cavalo");
+	  }
+	  
+	  
+  }
 
 }
