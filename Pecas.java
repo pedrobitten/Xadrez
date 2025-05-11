d665b31310f8fef9672fb84653448f4de503091f
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-05-11 11:48:52 -0300

    Update Pecas.java

--- Pecas.java
+++ Pecas.java
@@ -3,4 +3,9 @@ public abstract class Pecas {
   public abstract String getPeca();
   
   public abstract int getJogador();
+  
+  public abstract void movimento(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_destino);
+
+  public abstract void ataque(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_peca_inimiga);
+  
 }
