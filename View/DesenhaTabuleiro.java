86eb84a4bba6e6100a5c75609aee421377d9804d
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-05-21 17:53:16 -0300

    Update DesenhaTabuleiro.java

--- View/DesenhaTabuleiro.java
+++ View/DesenhaTabuleiro.java
@@ -1,14 +1,63 @@
 package View;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import Model.Bispo;
+import Model.Cavalo;
+import Model.Rainha;
+import Model.Rei;
+import Model.Torre;
+
 import java.awt.*;
 import java.awt.geom.*;
+import java.io.File;
+import java.io.IOException;
+
+
 
 public class DesenhaTabuleiro extends JPanel {
 
+	private Image []imagens_peca;
+	private String[] nomes_pecas = {
+			"b_torre", "b_cavalo", "b_bispo", "b_dama", "b_rei", "b_bispo", "b_cavalo", "b_torre",
+		    "b_peao", "b_peao", "b_peao", "b_peao", "b_peao", "b_peao", "b_peao", "b_peao",
+		    "p_peao", "p_peao", "p_peao", "p_peao", "p_peao", "p_peao", "p_peao", "p_peao",
+		    "p_torre", "p_cavalo", "p_bispo", "p_dama", "p_rei", "p_bispo", "p_cavalo", "p_torre"
+	};
+	
+	public DesenhaTabuleiro() {
+		carregarImagens();
+
+	}
+	
+	private void carregarImagens() {
+		imagens_peca = new Image[32];
+		
+		for (int i = 0; i < 32; i++)
+		{
+			String caminho = "D:/java_2025.1/Pecas/Pecas_1/" + nomes_pecas[i] + ".gif";
+			//System.out.print(caminho);
+			//System.out.println("\n");
+			
+			try {
+				imagens_peca[i] = ImageIO.read(new File(caminho));
+			}
+			
+			catch(IOException e) {
+				System.out.println(e.getMessage());
+				System.exit(1);
+			}
+		}
+	
+	}
+	
+	
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
+		 int tileWidth = getWidth() / 8;
+	     int tileHeight = getHeight() / 8;
 		
 		//Desenha linhas horizontais
 		for (int i = 0; i <= 8; i++)
@@ -52,7 +101,46 @@ public class DesenhaTabuleiro extends JPanel {
 			}
 			
 			g2d.fill(casa);
-			
 		}
+		
+		//Posicionar pecas
+		
+		//Pecas jogador2
+		
+		int linha = 0;
+		int coluna;
+		
+		for (coluna = 0; coluna < 8; coluna++)
+		{
+			g2d.drawImage(imagens_peca[24 + coluna], coluna * tileWidth, linha * tileHeight, tileWidth, tileHeight, null);
+		}
+		
+		//Peao jogador2
+		
+		linha = 1;
+		
+		for (coluna = 0; coluna < 8; coluna++)
+		{
+			g2d.drawImage(imagens_peca[16 + coluna], coluna * tileWidth, linha * tileHeight, tileWidth, tileHeight, null);
+		}
+		
+		//Pecas jogador1
+		
+		linha = 7;
+		
+		for (coluna = 0; coluna < 8; coluna++)
+		{
+			g2d.drawImage(imagens_peca[coluna], coluna * tileWidth, linha * tileHeight, tileWidth, tileHeight, null);
+		}
+		
+		//Peao jogador1
+		
+		linha = 6;
+		
+		for (coluna = 0; coluna < 8; coluna++)
+		{
+			g2d.drawImage(imagens_peca[8 + coluna], coluna * tileWidth, linha * tileHeight, tileWidth, tileHeight, null);
+		}
+		
 	}
 }
