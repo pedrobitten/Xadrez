d313312f3320ab13077a886661074f5b2124c23c
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-05-22 14:50:58 -0300

    Add files via upload

---
+++ View/JanelaInicial.java
@@ -0,0 +1,35 @@
+package View;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class JanelaInicial extends JFrame {
+	
+	public JanelaInicial(String titulo) {
+		super(titulo);
+		
+		JButton iniciar_jogo = new JButton("Iniciar Jogo");
+		JButton continuar_jogo = new JButton("Continuar Jogo"); //Apenas na 4ª Iteração
+		JPanel p = new JPanel();
+		
+		iniciar_jogo.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				
+				Frame tabuleiro = new Frame();
+				tabuleiro.setTitle("Xadrez");
+		    	tabuleiro.setVisible(true);
+			}
+		});
+		
+		
+		p.add(iniciar_jogo);
+		p.add(continuar_jogo);
+		
+		//p.setBackground(Color.WHITE);
+		getContentPane().add(p);
+		setSize(400,300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+	
+}
