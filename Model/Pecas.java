834fd41b0a9e5bf82bbf38c4f57f5400cd9f1520
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-05-30 18:43:37 -0300

    Update Pecas.java

--- Model/Pecas.java
+++ Model/Pecas.java
@@ -1,12 +1,20 @@
 package Model;
+import java.awt.*;
+
 public abstract class Pecas {
+	
+	protected Image imagem;
+	
+	public Image getImage() {
+		return imagem;
+	}
 
-  public abstract String getPeca();
+	public abstract String getPeca();
   
-  public abstract String getCor();
+	public abstract String getCor();
   
-  public abstract void movimento(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_destino);
+	public abstract void movimento(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_destino);
 
-  public abstract void ataque(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_peca_inimiga);
+	public abstract void ataque(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_peca_inimiga);
   
 }
