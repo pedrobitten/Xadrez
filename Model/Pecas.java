88ce8744544696ccdebbe6f1f4b05178a15756e7
pbittencourt <117097056+pedrobitten@users.noreply.github.com>
2025-06-14 19:25:18 -0300

    Update Pecas.java

--- Model/Pecas.java
+++ Model/Pecas.java
@@ -13,8 +13,11 @@ public abstract class Pecas {
   
 	public abstract String getCor();
   
-	public abstract void movimento(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_destino);
+	//public abstract void movimento(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_destino);
 
+	public abstract boolean movimentoValido(Tabuleiro tabuleiro, int linha_peca, int coluna_peca, int linha_destino, int coluna_destino);
+	
 	public abstract void ataque(Tabuleiro tabuleiro, String coordenada_peca, String coordenada_peca_inimiga);
+	
   
 }
