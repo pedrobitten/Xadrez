4248ddccf5979df0158e855f02c1b29f58c6a892
geovaneedias <geovaneedias@gmail.com>
2025-06-15 17:59:31 -0300

    Add files via upload

---
+++ Model/XadrezFacade.java
@@ -0,0 +1,52 @@
+package Model;
+
+import View.*;
+import Controller.*;
+
+public class XadrezFacade {
+    private Tabuleiro tabuleiro;
+    private Control control;
+    private DesenhaTabuleiro view;
+
+    public XadrezFacade() {
+        this.tabuleiro = new Tabuleiro();
+        this.control = Control.getController();
+        this.view = new DesenhaTabuleiro();
+    }
+
+    // Método para inicializar o jogo
+    public void iniciarJogo() {
+        JanelaInicial janela = new JanelaInicial("Xadrez");
+        janela.setVisible(true);
+    }
+
+    public boolean moverPeca(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino) {
+        Pecas peca = tabuleiro.matriz[linhaOrigem][colunaOrigem];
+        if (peca == null || !peca.movimentoValido(tabuleiro, linhaOrigem, colunaOrigem, linhaDestino, colunaDestino)) {
+            return false;
+        }
+
+        tabuleiro.matriz[linhaDestino][colunaDestino] = peca;
+        tabuleiro.matriz[linhaOrigem][colunaOrigem] = null;
+
+        view.repaint();
+
+        return true;
+    }
+
+    public boolean isXeque(String corRei) {
+        int[] posicaoRei = (corRei.equals("branco")) 
+            ? control.getPosicaoReiJogador1() 
+            : control.getPosicaoReiJogador2();
+        
+        return control.xequeRei(posicaoRei[0], posicaoRei[1], corRei) > 0;
+    }
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    public void atualizarView() {
+        view.repaint();
+    }
+}
